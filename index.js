b8aa867e713d0affb7ecf5c2e00bb12bc17556f5
Jay <chunsaa@gmail.com>
2019-12-31 00:02:47 -0500

    add authentication with bcrypt and set cookie session on valid login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieSession = require('cookie-session');
 
 const auth = require('./routes/auth');
 const checkout = require('./routes/checkout');
@@ -10,6 +11,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ["yolo"],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 app.use('/auth', auth);
 app.use('/checkout', checkout);
