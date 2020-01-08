891dd32606f22f9557393cdbc21dfd65061f1d50
Matt Wardle <mwardle.22@gmail.com>
2020-01-08 13:04:51 -0500

    Add route for specific job detail

--- routes/allJobs.js
+++ routes/allJobs.js
@@ -3,18 +3,33 @@ const router = express.Router();
 const { pool } = require('../config');
 
 router.get('/', (req, res) => {
-  console.log(req.body)
-  const queryString = `SELECT * FROM jobs WHERE jobber_id IS NULL AND is_deleted=false`;
-  pool.query(queryString, (error, results) => {
-    if (error) {
-      throw error
-    }
-    if (results.rows.length > 0) {
-      res.json(results.rows)
-    } else {
-      console.log(results)
-    }
-  })
+  if (req.query.id) {
+    console.log("id found", req.query.id)
+    const queryString = `SELECT * FROM jobs WHERE id=${req.query.id}`;
+    pool.query(queryString, (error, results) => {
+      if (error) {
+        throw error
+      }
+      if (results.rows.length > 0) {
+        res.json(results.rows)
+      } else {
+        console.log(results)
+      }
+    })
+  } else {
+    console.log("no id found")
+    const queryString = `SELECT * FROM jobs WHERE jobber_id IS NULL AND is_deleted=false`;
+    pool.query(queryString, (error, results) => {
+      if (error) {
+        throw error
+      }
+      if (results.rows.length > 0) {
+        res.json(results.rows)
+      } else {
+        console.log(results)
+      }
+    })
+  }
 })
 
 router.put('/:id', (req, res) => {
