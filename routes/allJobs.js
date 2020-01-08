5492075cc26b058bb43b9539241683159070b277
Matt Wardle <mwardle.22@gmail.com>
2020-01-08 11:49:11 -0500

    Implement job accepting functionality

--- routes/allJobs.js
+++ routes/allJobs.js
@@ -17,4 +17,25 @@ router.get('/', (req, res) => {
   })
 })
 
+router.put('/:id', (req, res) => {
+  const jobId = req.params.id
+  const queryString = `
+  UPDATE jobs SET jobber_id=${req.session.userId}
+  WHERE id=${jobId}
+  `
+  const id = [req.body[0]]
+  pool.query(queryString, (error, results) => {
+    // if (error) {
+    //   res.json({
+    //     result: false
+    //   });
+    //   res.json({
+    //     result: true,
+    //     message: "job accepted"
+    //   })
+    // }
+  })
+  res.send('okay')
+})
+
 module.exports = router;
