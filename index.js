448fb667ea885fd864f22f205832f398829cd764
Dan <hexular@gmail.com>
2020-01-08 13:12:12 -0500

    add websockets foundation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const app = express();
+const http = require("http");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieSession = require('cookie-session');
@@ -6,12 +8,28 @@ const jobs = require('./routes/jobs');
 const auth = require('./routes/auth');
 const checkout = require('./routes/checkout');
 const skills = require('./routes/skills');
+const server = http.createServer(app);
 
-const app = express();
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  console.log('connected')
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+// app.options('*:*', cors())
 
 app.use(cookieSession({
   name: 'session',
@@ -25,6 +43,7 @@ app.use('/auth', auth);
 app.use('/checkout', checkout);
 app.use('/skills', skills);
 app.use('/myjobs', jobs);
+
 
 // Start server
 const port = process.env.PORT || 8001;
