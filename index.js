7db4c33916068a4b1ea6247d41b219af3101924f
Jay <chunsaa@gmail.com>
2020-01-10 16:09:10 -0500

    t

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ wss.on('connection', function connection(ws) {
     });
   });
 });
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
